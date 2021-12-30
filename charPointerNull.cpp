8b04fe7deb9ba5eb3acf3bc6cb162d954f62ddf0
kumar031 <66525068+kumar031@users.noreply.github.com>
2021-12-30 14:06:32 +0530

    Update charPointerNull.cpp

--- charPointerNull.cpp
+++ charPointerNull.cpp
@@ -1,8 +1,9 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 void checkNull(char* c){
-     //if (!c)
-    if (c==nullptr)
+    //Use if(roadNumber == NULL) to test if roadNumber is NULL 
+    //and use if(*roadNumber = '\0') to test if roadNumber is empty.
+    if (c==nullptr) //or  if (!c)
     {
         std::cout<<"got null pointer"<<std::endl;
     }else if (c=="")
@@ -13,6 +14,7 @@ void checkNull(char* c){
          std::cout<<"got string "<<std::endl;
     }
 }
+
 int main() {
    // checkNull(nullptr);
     checkNull("");//inverted comma is not nullpointer
